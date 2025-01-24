cc85585a4825a1905c1c8d5a0b95c822118dd0eb
Dave White <joltedbot@gmail.com>
2025-01-23 22:18:43 -0800

    Added a simple way to pass it test wave files

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,25 @@ mod wave;
 
 use crate::fileio::read_bytes_from_file_as_string;
 use crate::wave::Wave;
+use std::env::Args;
 use std::fs::File;
+use std::process::exit;
 
 const FILE_CHUNKID_LENGTH_IN_BYTES: usize = 4;
 const WAVE_FILE_CHUNKID: &str = "RIFF";
 
 fn main() {
-    let path_of_file_to_read = "./test/from_reaper.wav".to_string();
+    let mut argv: Args = std::env::args();
+
+    let path_of_file_to_read: String = {
+        if argv.len() > 1 {
+            argv.nth(1).expect("Failed to read the 1st parameter")
+        } else {
+            println!("Please provide a path to a file to read.");
+            exit(1);
+        }
+    };
+
     let mut file = File::open(&path_of_file_to_read).expect("Could not open file path");
 
     let file_chunk_id = read_bytes_from_file_as_string(&mut file, FILE_CHUNKID_LENGTH_IN_BYTES)
